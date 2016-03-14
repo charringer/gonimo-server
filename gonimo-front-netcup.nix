8a134ca297c651ef9167b08ec155b57600825bd3
Georg Pichler <georg.pichler@gmail.com>
2016-03-14 14:51:18 +0100

    "none" backend configuration for netcup server

---
+++ gonimo-front-netcup.nix
@@ -0,0 +1,25 @@
+{
+  gonimo-front =
+    { config, pkgs, ...}:
+    {
+      deployment.targetEnv = "none";
+
+      # Host specifics
+      boot.loader.grub.device = "/dev/vda";
+      imports =
+      [ <nixpkgs/nixos/modules/profiles/qemu-guest.nix>
+      	];
+
+      boot.initrd.availableKernelModules = [ "ata_piix" "uhci_hcd" "virtio_pci" "virtio_blk" ];
+      boot.kernelModules = [ ];
+      boot.extraModulePackages = [ ];
+
+      fileSystems."/" =
+      { device = "/dev/disk/by-label/nixos";
+      	fsType = "ext4";
+	};
+	
+      services.openssh.enable = true;
+      networking.firewall.allowPing = true;
+    };
+}
